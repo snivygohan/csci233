192f908bfd1bf3a249139dac2857ed624081a4a8
Jule Dimaranan <julejao@live.com>
2023-10-13 01:07:51 -0400

    Got a register page working, connected to login page

--- login/urls.py
+++ login/urls.py
@@ -4,4 +4,7 @@ from . import views
 #URL config
 urlpatterns = [
     path('login/', views.login_test),
+    path('register/',views.register_user),
+
 ]
+
--- login/views.py
+++ login/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+from .forms import CreateUserForm
 
 def login_test(request):
     return render(request, 'index.html')
+
+def register_user(request):
+    form = CreateUserForm
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form':form}
+    return render(request, 'register.html', context)
