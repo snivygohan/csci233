e27d2770eb65f0558ebcdb4c8f6bce71b4dca576
Kevin Vu <kevin.vu64@myhunter.cuny.edu>
2023-10-16 00:49:41 -0400

    rough code of ORM in comments for login info

--- login/models.py
+++ login/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# still learning the ORM part of django, so i put my code here in comment(s) -kev
+#class LoginInfo(models.Model):
+#    username = models.CharField(max_Length=25)
+#    email = models.CharField(max_Length=100)
+#    password1 = models.CharField(max_Length=50)
+
+#going to need to confer with steve to see how to hook up this model to admin and then our postgres db later. -kev
