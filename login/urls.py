869bdb40fd4a63c8aabbc3121ad40381cd01eae7
Jule Dimaranan <julejao@live.com>
2023-11-01 12:41:49 -0400

    Update What Steven Did

--- login/urls.py
+++ login/urls.py
@@ -3,10 +3,15 @@ from . import views
 
 #URL config
 urlpatterns = [
-    path('home/', views.home_page, name='home'),
+    path('', views.home_page, name='home'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+<<<<<<< HEAD
     path('register/',views.register_user, name='register'),
     path('profile/', views.profile, name = 'profile')
+=======
+    path('register/', views.register_user, name='register'),
+    path('addgame/', views.add_game, name='addgame')
+>>>>>>> origin/testSteve
 ]
 
