cfbfaec102431cbf1a6cb3c57350d4e6c7662e1c
lilkevo58 <kevin.vu64@myhunter.cuny.edu>
2023-11-03 14:08:49 -0400

    skeleton of dynamic url is working
    i'll work on the cleanup and formatting of the all games page later. :D

--- gamesPage/urls.py
+++ gamesPage/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+#remember that you add "/games/" after whatever is there for the URL, and then continue the pathing.
+
 urlpatterns = [
-    path('game/<int:id>/', views.games_page, name = 'game'),
-    path('all/', views.all_games, name = 'all')
+    path('all/', views.all_games, name = 'all'),
+    path('game/<int:id>/', views.game_detail_view, name = 'game')
 ]
--- gamesPage/views.py
+++ gamesPage/views.py
@@ -11,8 +11,10 @@ def all_games(request):
     games_list = Games.objects.all()[:10]
     return render(request, 'allGames.html', {'games_list':games_list})
 
-def game(request, id):
-    id = Games.objects.get(id=id)
-    game = Games.objects.all()
-    context = {'id':id}, {'game':game}
+def game_detail_view(request, id=None):
+    gameDetails=None
+    if id is not None:
+        gameDetails = Games.objects.get(id=id)
+    #game = Games.objects.all()
+    context = {"object":gameDetails,}
     return render(request, 'games.html', context)
