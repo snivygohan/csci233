3c964d2957894ff3124f78fe18fbcc788e9c3840
Steve Deng <stevedeng0@gmail.com>
2023-11-07 16:54:17 -0500

    Added admin button + 2 field to collections model
    Admin button only visible to superusers

--- baseApp/models.py
+++ baseApp/models.py
@@ -4,19 +4,18 @@ from django.conf import settings
 # Create your models here.
 
 class Games(models.Model):
+    class ESRB_Rating(models.TextChoices):
+        E = 'E'
+        E10 = 'E10+'
+        T = 'T'
+        M = 'M'
+        NORATING = 'NR'
 
-    ESRB = (
-        ('E', 'E'),
-        ('E10+', 'E10+'),
-        ('T', 'T'),
-        ('M', 'M'),
-        ('NR', 'NR')
-    )
     id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=255)
     release_date = models.CharField(db_column='Release Date', max_length=255)
     team = models.CharField(max_length=255)
-    esrb = models.CharField(max_length=255, choices=ESRB)
+    esrb = models.CharField(max_length=255, choices=ESRB_Rating.choices, default=ESRB_Rating.NORATING)
     platforms = models.CharField(max_length=255)
     multiplayer = models.BooleanField()
     genres = models.CharField(max_length=255)
@@ -31,8 +30,15 @@ class Games(models.Model):
         db_table = 'games'
 
 class Collections(models.Model):
+    class GameStatus(models.TextChoices):
+        PLAYING = 'Playing'
+        COMPLETED = 'Completed'
+        Dropped = 'Dropped'
+
     currentUser = models.ForeignKey(settings.AUTH_USER_MODEL, related_name= 'owner', null = True, on_delete= models.SET_NULL)  
     games = models.ForeignKey(Games, related_name='games', null = True, on_delete= models.SET_NULL)
+    status = models.CharField(max_length=10, blank= True, choices=GameStatus.choices)
+    favorite = models.BooleanField(default= False)
 
     def __str__(self):
         return f'({self.currentUser}-{self.games})'
