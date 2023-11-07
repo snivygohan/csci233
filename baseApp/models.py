9b21629f1e4c20ef1db68776496d3a0ff6f20774
Steve Deng <stevedeng0@gmail.com>
2023-11-06 19:48:48 -0500

    Moved collections model to baseApp

--- baseApp/models.py
+++ baseApp/models.py
@@ -30,6 +30,28 @@ class Games(models.Model):
         managed = False
         db_table = 'games'
 
+class Collections(models.Model):
+    currentUser = models.ForeignKey(User, related_name= 'owner', null = True, on_delete= models.SET_NULL)  
+    games = models.ForeignKey(Games, related_name='games', null = True, on_delete= models.SET_NULL)
+
+    def __str__(self):
+        return self.currentUser.__str__()
+    
+    class Meta:
+        managed = True
+        db_table = 'collections'
+        verbose_name_plural = 'Collections'
+
+    @classmethod
+    def addGame(cls, currentUser, newGame):
+        collection, created = cls.objects.get_or_create( currentUser = currentUser )
+        collection.games.add(newGame)
+
+    @classmethod
+    def removeGame(cls, currentUser, newGame):
+        collection, created = cls.objects.get_or_create( currentUser = currentUser )
+        collection.games.remove(newGame)
+
 class Completed(models.Model):
     user_id = models.ForeignKey(User, models.DO_NOTHING, blank=True, null=True)
     game_ids = models.ForeignKey(Games, models.DO_NOTHING, db_column='game_ids', blank=True, null=True)
