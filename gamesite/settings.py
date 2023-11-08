51d108869dd179e8881733b3152ad710a0de037c
Jule Dimaranan <julejao@live.com>
2023-11-08 12:42:47 -0500

    Fix collecitons issue

--- gamesite/settings.py
+++ gamesite/settings.py
@@ -95,13 +95,14 @@ WSGI_APPLICATION = 'gamesite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT')
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD':'school1',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
